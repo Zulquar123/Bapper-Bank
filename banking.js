b0970c1b364569653093cc665874b83d3c2531fa
Zulquar123 <zulquar150@gmail.com>
2022-02-10 19:30:48 +0300

    update code

--- banking.js
+++ banking.js
@@ -1,6 +1,10 @@
 // handle deposit button event
-document.getElementById('deposit-submit').addEventListener('click', function () {
-    //Get user deposit
-    const depositFeild = document.getElementById('deposit-amount');
-    const userDeposit = depositFeild.value;
-});
+document.getElementById('deposit-button').addEventListener('click', function () {
+    //Get the amount deposited
+    const depositFeild = document.getElementById('deposit-input');
+    const depositAmount = depositFeild.value;
+
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmount;
+    console.log(depositAmount);
+})
