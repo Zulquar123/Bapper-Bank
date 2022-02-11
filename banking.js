ec76303e4573dcceed1cdc950a57c86f7a952f20
Zulquar123 <zulquar150@gmail.com>
2022-02-11 18:21:28 +0300

    update code

--- banking.js
+++ banking.js
@@ -1,10 +1,45 @@
 // handle deposit button event
 document.getElementById('deposit-button').addEventListener('click', function () {
+
     //Get the amount deposited
     const depositFeild = document.getElementById('deposit-input');
-    const depositAmount = depositFeild.value;
+    const newDepositAmountText = depositFeild.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+    // Update Deposit Total
+
+
+    depositFeild.value = '';  // Clear the deposite Field
+
+
+    // Update Balance Account
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
 
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
-    console.log(depositAmount);
-})
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+    depositTotal.innerText = newDepositTotal;
+});
+
+// handle withdraw button event
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+
+    //Get the amount deposited
+    const withdrawFeild = document.getElementById('Withdraw-input');
+    const newWithdrawAmountText = withdrawFeild.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+});
+
